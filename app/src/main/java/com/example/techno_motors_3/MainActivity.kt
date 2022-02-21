2e2bf5834cf3cba1d35fd0caf1ce13b2ebdf5f64
Harrier55 <harrier55@yandex.ru>
2022-02-21 22:57:59 +0500

    Initial commit

---
+++ app/src/main/java/com/example/techno_motors_3/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.techno_motors_3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.techno_motors_3.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+
+
+}
