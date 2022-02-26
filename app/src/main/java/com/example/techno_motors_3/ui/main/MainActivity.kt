794aa8cd618cbb79e76b9582af8ffec6b5271d84
Harrier55 <harrier55@yandex.ru>
2022-02-26 17:38:25 +0500

    add Action Bar Title

--- app/src/main/java/com/example/techno_motors_3/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/techno_motors_3/ui/main/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.techno_motors_3.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.techno_motors_3.R
 import com.example.techno_motors_3.databinding.ActivityMainBinding
@@ -17,11 +17,13 @@ class MainActivity : AppCompatActivity() {
     private val autoFragment by lazy { AutoFragment() }
     private val serviceFragment by lazy { ServiceFragment() }
 
+    private val actionBar by lazy { this.supportActionBar}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        actionBar?.setTitle(R.string.app_name)
 
         launchFragment(homeFragment)
         initBottomNavigation()
@@ -41,17 +43,21 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.menu_home -> {
                     launchFragment(homeFragment)
+                    actionBar?.setTitle(R.string.app_name)
                     true
                 }
                 R.id.menu_auto -> {
                     launchFragment(autoFragment)
+                    actionBar?.setTitle(R.string.menu_auto)
                     true
                 }
                 R.id.menu_service -> {
                     launchFragment(serviceFragment)
+                    actionBar?.setTitle(R.string.menu_service)
                     true
                 }
                 R.id.menu_contacts -> {
+                    actionBar?.setTitle(R.string.menu_contacts)
                     true
                 }
                 else -> false
