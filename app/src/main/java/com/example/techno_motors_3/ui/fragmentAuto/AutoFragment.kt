6cf76bd1d64aa869c048c70471b00554885dee31
Harrier55 <harrier55@yandex.ru>
2022-02-27 00:16:53 +0500

    add listAdapter

--- app/src/main/java/com/example/techno_motors_3/ui/fragmentAuto/AutoFragment.kt
+++ app/src/main/java/com/example/techno_motors_3/ui/fragmentAuto/AutoFragment.kt
@@ -1,14 +1,18 @@
 package com.example.techno_motors_3.ui.fragmentAuto
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.ListFragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.techno_motors_3.R
+import com.example.techno_motors_3.databinding.AutoFragmentBinding
 
-class AutoFragment : Fragment() {
+class AutoFragment : ListFragment() {
+
+    private lateinit var binding:AutoFragmentBinding
 
     companion object {
         fun newInstance() = AutoFragment()
@@ -16,11 +20,20 @@ class AutoFragment : Fragment() {
 
     private lateinit var viewModel: AutoViewModel
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.auto_fragment, container, false)
+       val view = inflater.inflate(R.layout.auto_fragment, container, false)
+        binding = AutoFragmentBinding.bind(view)
+        val mItem = resources.getStringArray(R.array.item_menu_car_fragment)
+
+        val arrayAdapter = ArrayAdapter<String>(requireContext(),android.R.layout.simple_list_item_1,mItem)
+        binding.list.adapter = arrayAdapter
+
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
